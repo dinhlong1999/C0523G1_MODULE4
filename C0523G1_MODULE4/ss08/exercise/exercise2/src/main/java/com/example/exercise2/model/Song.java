2a4a7b0cae916b99f1621da19d0fc2aa35ddc8c3
dinhlong1999 <long.truong.vcr@gmail.com>
2023-09-27 13:51:32 +0700

    long

--- C0523G1_MODULE4/ss08/exercise/exercise2/src/main/java/com/example/exercise2/model/Song.java
+++ C0523G1_MODULE4/ss08/exercise/exercise2/src/main/java/com/example/exercise2/model/Song.java
@@ -1,9 +1,6 @@
 package com.example.exercise2.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -12,13 +9,13 @@ public class Song {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NotNull
+    @Column(columnDefinition = "varchar(255) not null")
     private String name;
 
-    @NotNull
+    @Column(columnDefinition = "varchar(255) not null")
     private String singer;
 
-    @NotNull
+    @Column(columnDefinition = "varchar(255) not null")
     private String kindOfMusic;
 
     public Song(int id, String name, String singer, String kindOfMusic) {
